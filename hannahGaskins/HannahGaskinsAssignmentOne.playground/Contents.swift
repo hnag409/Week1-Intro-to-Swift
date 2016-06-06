a5941c2516b262ab53877865590e7b43e98d12e4
hannah gaskins <hannahgaskins@hannahs-MacBook-Pro.local>
2016-06-06 15:06:12 -0700

    challenges 3 4 and bonus

--- hannahGaskins/HannahGaskinsAssignmentOne.playground/Contents.swift
+++ hannahGaskins/HannahGaskinsAssignmentOne.playground/Contents.swift
@@ -1,6 +1,14 @@
-// Week One, Assignment One 6/6/16
+/* 
+ 
+ =================== Week One, Assignment One 6/6/16 ===================
+ 
+*/
 
-// Write a function that takes in a string, checks if it can be converted to a number and if possible, returns a number
+
+
+// 1. Write a function that takes in a string, checks if it can be converted to a number and if possible, returns a number
+
+
 
 func stringToInt(string: String) -> Int? {
     
@@ -13,7 +21,11 @@ stringToInt(numbers)
 var test = "hiya"
 stringToInt(test)
 
-// Write a function that takes in a tuple (code, message) and returns a string by using interpolation
+
+
+// 2. Write a function that takes in a tuple (code, message) and returns a string by using interpolation
+
+
 
 func tupleToString(code: String, message: String) -> String {
     return "The code is \(code), and the message is \(message)"
@@ -21,5 +33,51 @@ func tupleToString(code: String, message: String) -> String {
 
 tupleToString("Mary Poppins", message: "bibidy bobidy boop")
 
-// Write a function that take in a string, and returns a message: “String contains n characters”
+
+
+// 3. Write a function that take in a string, and returns a message: “String contains n characters”
+
+
+
+func characterCount(text: String) -> String {
+    let n = text.characters.count
+    return "String contains \(n) characters"
+}
+
+characterCount("Hiya")
+characterCount("")
+characterCount("!@#$%^&*()_+}{|?>:~")
+characterCount("123456789")
+
+
+
+// 4. Write a function that takes in a string and reverses it. Use a simple for loop
+
+
+func stringReverse(stringToReverse: String) -> String {
+    var newString = ""
+    
+    for i in stringToReverse.characters {
+        newString = "\(i)" + newString
+    }
+    return newString
+}
+
+stringReverse("hiya Laura, let's get steps this summer!")
+
+
+// Bonus: Declare a variable that’s function. Implement function to take in a string (name) and return greeting message.
+
+
+func greetingMessage(name: String) -> String {
+    return "Hiya, \(name)"
+}
+
+var returnedGreet: (String) -> String = greetingMessage
+
+returnedGreet("Hannah")
+
+
+
+
 
