d7cbdb6b3d56b92cdd630422d0a7d0d3b0c52f1d
hannah gaskins <hannahgaskins@hannahs-MacBook-Pro.local>
2016-06-07 12:54:39 -0700

    added question one

--- hannahGaskins/HanahGaskinsAssignmentTwo.playground/Contents.swift
+++ hannahGaskins/HanahGaskinsAssignmentTwo.playground/Contents.swift
@@ -3,3 +3,6 @@
  =================== Week One, Assignment Two 6/7/16 ===================
  
  */
+
+// 1. Write a function that takes in an array of strings, checks if array contains “Earth”, if not, adds it otherwise returns
+
